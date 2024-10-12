bcbfb9d773a572d96b139c4259cc8c9ab49c1fbd
j4n7 <garciagutierrez@gmail.com>
2024-10-12 23:16:31 +0200

    Porygon pokeball

--- maps/Office.asm
+++ maps/Office.asm
@@ -12,10 +12,14 @@ Office_MapScriptHeader:
 	def_object_events
 	object_event  3,  4, SPRITE_SILPH_EMPLOYEE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OfficeEmployee1Script, -1
 	object_event  3,  9, SPRITE_SILPH_EMPLOYEE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OfficeEmployee2Script, -1
+	object_event 16, 14, SPRITE_SILPH_EMPLOYEE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OfficeEmployee3Script, -1
+	object_event  5, 15, SPRITE_BALL_CUT_FRUIT, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_POKE_BALL, OBJECTTYPE_SCRIPT, 0, PorygonBallScript, -1
 
 	object_const_def
 	const OFFICE_EMPLOYEE_1
 	const OFFICE_EMPLOYEE_2
+	const OFFICE_EMPLOYEE_3
+	const PORYGON_BALL
 
 OfficeEmployee1Script:
 	faceplayer
@@ -32,6 +36,57 @@ OfficeEmployee2Script:
 	promptbutton
 	turnobject OFFICE_EMPLOYEE_2, UP
 	endtext
+
+OfficeEmployee3Script:
+	faceplayer
+	opentext
+	writetext OfficeEmployee3Text
+	promptbutton
+	turnobject OFFICE_EMPLOYEE_3, UP
+	endtext
+
+PorygonBallScript:
+	faceplayer
+	readvar VAR_PARTYCOUNT
+	ifequalfwd PARTY_LENGTH, .PartyFullGift
+	disappear PORYGON_BALL
+	opentext
+	writetext PorygonBallText
+	promptbutton
+	givepoke PORYGON, PLAIN_FORM, 20, ORAN_BERRY
+	closetext
+	end
+
+.PartyFullGift:
+	opentext
+	writetext PartyFullGiftText
+	waitbutton
+	closetext
+	end
+	
+PartyFullGiftText:
+	text "Party is full."
+
+	done
+	
+PorygonBallText:
+	text "Someone left this"
+	line "#MON for you."
+	
+	para "There is a label"
+	line "on the Ball:"
+	cont "<PLAYER>"
+
+	done
+	
+ReceivedGiftText:
+	text "<PLAYER> received"
+	line "@"
+
+	text_ram wStringBuffer3
+	text "!"
+
+	done
 	
 OfficeEmployee1Text:
 	text "Work, work, work."
@@ -42,5 +97,10 @@ OfficeEmployee1Text:
 OfficeEmployee2Text:
 	text "I'm busy,"
 	line "don't bother me."
+
+	done
+
+OfficeEmployee3Text:
+	text "I'm from Naljo."
 
 	done
